cf0647020c88dbd10794a3849b76069451980b7d
Beksultan <beksultankaiypov@gmail.com>
2020-09-20 18:39:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(board = ["O", "X", " ", " ", "X", " ", "X", "O", " "])
-  puts " #{board[0]   |   |   "
+  puts " #{board[0]}   |  #{board[1]}   | #{board[2]}   "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]}   |  #{board[0]}   | #{board[0]}   "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[0]}   |  #{board[0]}   | #{board[0]}   "
 end
